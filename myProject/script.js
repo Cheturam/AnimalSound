8840dffd37b007f485cae86b87eeef29e54bcb03
Chetan Minajigi <53223273+Cheturam@users.noreply.github.com>
2020-11-03 21:24:56 +0530

    Add files via upload

---
+++ myProject/script.js
@@ -0,0 +1,64 @@
+function rabPlay(){
+    var audioSound=document.getElementById("rabbit-sound");
+    audioSound.play();
+}
+function dinoPlay(){
+    var audioSound=document.getElementById("dinosaur-sound");
+    audioSound.play();
+}
+
+function cowPlay(){
+    var audioSound=document.getElementById("cow-sound");
+    audioSound.play();
+}
+
+function elepPlay(){
+    var audioSound=document.getElementById("elephant-sound");
+    audioSound.play();
+}
+
+function frogPlay(){
+    var audioSound=document.getElementById("frog-sound");
+    audioSound.play();
+}
+
+function whalPlay(){
+    var audioSound=document.getElementById("whale-sound");
+    audioSound.play();
+}
+function leopPlay(){
+    var audioSound=document.getElementById("leopard-sound");
+    audioSound.play();
+}
+function lionPlay(){
+    var audioSound=document.getElementById("lion-sound");
+    audioSound.play();
+}
+function rhioPlay(){
+    var audioSound=document.getElementById("rhinoceros-sound");
+    audioSound.play();
+}
+function roostPlay(){
+    var audioSound=document.getElementById("Rooster-sound");
+    audioSound.play();
+}
+function tigePlay(){
+    var audioSound=document.getElementById("tiger-sound");
+    audioSound.play();
+}
+function wolfPlay(){
+    var audioSound=document.getElementById("wolf-sound");
+    audioSound.play();
+}
+function alliPlay(){
+    var audioSound=document.getElementById("alligator-sound");
+    audioSound.play();
+}
+function apePlay(){
+    var audioSound=document.getElementById("ape-sound");
+    audioSound.play();
+}
+function catPlay(){
+    var audioSound=document.getElementById("cat-sound");
+    audioSound.play();
+}
